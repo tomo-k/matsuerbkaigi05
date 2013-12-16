531f341429a86424fbcdbf1f5461a6370d4efe62
Hiroyuki Inoue <syslink.h.inoue@gmail.com>
2013-12-16 23:05:12 +0900

    プログラムに午前の部を追加

--- matrk05/static/js/application.js
+++ matrk05/static/js/application.js
@@ -4,8 +4,6 @@ $(function() {
   $("a").click(function () {
     var mark = $(".mark").offset().top;
     var point = $($(this).attr("href")).offset().top;
-    console.log(mark)
-    console.log(point)
     $('html,body').animate({ scrollTop: point - 120 }, 'fast');
     return false;
   });
